8d6bb5cb26a5ea35acce98ee21efec88b0f36c62
Shashank Sinha <shashank.wmr@gmail.com>
2023-08-06 02:39:45 -0400

    Setting up the stock page routing

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {StockOverviewPage} from "./pages/StockOverviewPage";
+import {StockDetailPage} from "./pages/StockDetailPage";
+
+export default function App() {
+  return (
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<StockOverviewPage />} />
+          <Route path="/detail/:stock" element={<StockDetailPage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
+}
+
+
