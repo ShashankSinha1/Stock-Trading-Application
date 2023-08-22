b6db39594df9e34abeb9297eb2686e2e10eca85e
Shashank Sinha <shashank.wmr@gmail.com>
2023-08-22 14:07:33 -0400

    Imported the stock list and autocomplete search bar components

--- src/pages/StockOverviewPage.jsx
+++ src/pages/StockOverviewPage.jsx
@@ -1,3 +1,8 @@
+import { AutoCompleteBar } from "../components/AutoCompleteBar" 
+import { StockList } from "../components/StockList"
 export const StockOverviewPage = () =>{
-    return <div>Stock Overview Page</div>
+    return <div>Stock Overview Page
+        <AutoCompleteBar />
+        <StockList />
+    </div>
 }
