7e32abea5169c4d984b10f92a399cd78723216d7
Shashank Sinha <shashank.wmr@gmail.com>
2023-08-22 14:06:51 -0400

    Created the stock list component

---
+++ src/components/StockList.jsx
@@ -0,0 +1,6 @@
+import {useState, useEffect} from "react"
+export const StockList = () => {
+    /* defaults to an empty array, whatever you pass into the use state will be the default state */
+    useState([])
+    return <div>Stock List</div>
+}
