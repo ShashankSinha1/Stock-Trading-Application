bd73476aad5734375bcae8132d021407a6609778
Shashank Sinha <shashank.wmr@gmail.com>
2023-09-24 13:40:07 -0400

    Add search bar

--- src/components/AutoCompleteBar.jsx
+++ src/components/AutoCompleteBar.jsx
@@ -1,3 +1,9 @@
+import { BsSearch } from "react-icons/bs";
+
 export const AutoCompleteBar = () => {
-    return <div>AutoCompleteBar</div>
+    return <div className="w-50 p-5 rounded mx-auto">
+            <div clasName="form-floating ">
+            <input style={{backgroundColor: "rgba(145, 158, 171, 0.04"}} id="search" type="text" className="form-control" placeholder="Search"/>
+        </div>
+    </div>
 }
