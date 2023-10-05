ef1ab21512d701e72e1711cd58ef9cb9959ca684
Shashank Sinha <shashank.wmr@gmail.com>
2023-10-05 01:11:58 -0400

    Integrates WatchListContextProvider into the main App component. The context provider is wrapped around the app's routing, making it accessible to child components for managing a watchlist of stocks.

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {StockOverviewPage} from "./pages/StockOverviewPage";
 import {StockDetailPage} from "./pages/StockDetailPage";
-
+import { WatchListContextProvider } from './context/watchListContext';
 
 
 export default function App() {
   return (
     <main className='container'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<StockOverviewPage />} />
-          <Route path="/detail/:stock" element={<StockDetailPage/>}/>
-        </Routes>
-      </BrowserRouter>
+      <WatchListContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<StockOverviewPage />} />
+            <Route path="/detail/:stock" element={<StockDetailPage/>}/>
+          </Routes>
+        </BrowserRouter>
+      </WatchListContextProvider>
     </main>
   );
 }
